157fc2c08f4a0c616659ea9abfe663ab006d202a
dardo200 <dardo200@gmail.com>
2025-03-21 18:28:15 -0300

    Correcciones param

--- app/api/clientes/[id]/historial/route.ts
+++ app/api/clientes/[id]/historial/route.ts
@@ -3,12 +3,13 @@ import { obtenerHistorialComprasCliente } from "@/lib/cliente-service"
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
   try {
-    const id = Number.parseInt(params.id)
+    const paramsAwait = await params
+    const id = Number.parseInt(paramsAwait.id)
     const historial = await obtenerHistorialComprasCliente(id)
 
     return NextResponse.json(historial)
   } catch (error) {
-    console.error(`Error al obtener historial de compras del cliente con id ${params.id}:`, error)
+    console.error(`Error al obtener historial de compras del cliente con id ${(await params).id}:`, error)
     return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 })
   }
 }
